be8cb0a8e100b8f1c9fe707b7f16a68b9a40cb3b
U-riani <sandropapiashvili@gmail.com>
2025-12-15 16:10:34 +0400

    update url

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -15,7 +15,7 @@ import {BRAND_NAMES} from "../data/brands";
 
 
 const HomePage = () => {
-  const baseURL = "https://gtex-sms-verification-server-b56rj7jaj-u-rianis-projects.vercel.app";
+  const baseURL = "https://gtex-sms-verification-server-lcrbftmui-u-rianis-projects.vercel.app";
   // const baseURL = "http://localhost:5000";
   const initialFields = {
     brands: [],
@@ -305,7 +305,7 @@ const HomePage = () => {
       });
 
       const data = await res.json();
-
+console.log(data);
       if (!data.success) {
         setErrors((prev) => ({
           ...prev,
