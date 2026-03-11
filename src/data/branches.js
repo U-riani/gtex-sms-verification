b89fcb2ee96ecd918e6a1f726780c4ee9852f11c
U-riani <sandropapiashvili@gmail.com>
2026-03-11 17:24:34 +0400

    s

--- src/data/branches.js
+++ src/data/branches.js
@@ -115,18 +115,236 @@ export const branches = [
     brand: "English Home",
   },
   {
-    id: 22,
+    id: 23,
     name: "English Home Grand Mall",
     brand: "English Home",
   },
   {
-    id: 22,
+    id: 24,
     name: "English Home East Point",
     brand: "English Home",
   },
   {
-    id: 22,
+    id: 25,
     name: "English Home City Mall",
     brand: "English Home",
   },
+  {
+    id: 26,
+    name: "Enza Home Agmashenebeli",
+    brand: "Enza Home",
+  },
+  {
+    id: 27,
+    name: "Enza Home East Point",
+    brand: "Enza Home",
+  },
+  {
+    id: 28,
+    name: "Enza Home City Mall",
+    brand: "Enza Home",
+  },
+  {
+    id: 29,
+    name: "Enza Home Grand Mall",
+    brand: "Enza Home",
+  },
+  {
+    id: 30,
+    name: "Terranova Agmashenebeli",
+    brand: "Terranova",
+  },
+  {
+    id: 31,
+    name: "Terranova East Point",
+    brand: "Terranova",
+  },
+  {
+    id: 32,
+    name: "Terranova Metro City",
+    brand: "Terranova",
+  },
+  {
+    id: 33,
+    name: "Terranova Tbilisi Central",
+    brand: "Terranova",
+  },
 ];
+
+// export const branches = [
+//   {
+//     id: 1,
+//     name: "https://gtex-sms-verification.netlify.app/Principe%20Vake",
+//     brand: "Principe",
+//   },
+//   {
+//     id: 2,
+//     name: "https://gtex-sms-verification.netlify.app/Penti%20Tbilisi%20Mall",
+//     brand: "Penti",
+//   },
+//   {
+//     id: 3,
+//     name: "https://gtex-sms-verification.netlify.app/Penti%20Rustavi",
+//     brand: "Penti",
+//   },
+//   {
+//     id: 4,
+//     name: "https://gtex-sms-verification.netlify.app/Penti%20Pekini",
+//     brand: "Penti",
+//   },
+//   {
+//     id: 5,
+//     name: "https://gtex-sms-verification.netlify.app/Penti%20Metro%20City",
+//     brand: "Penti",
+//   },
+//   {
+//     id: 6,
+//     name: "https://gtex-sms-verification.netlify.app/Penti%20Kutaisi",
+//     brand: "Penti",
+//   },
+//   {
+//     id: 7,
+//     name: "https://gtex-sms-verification.netlify.app/Penti%20Grand%20Mall",
+//     brand: "Penti",
+//   },
+//   {
+//     id: 8,
+//     name: "https://gtex-sms-verification.netlify.app/Penti%20Metro%20City",
+//     brand: "Penti",
+//   },
+//   {
+//     id: 9,
+//     name: "https://gtex-sms-verification.netlify.app/Penti%20Gori",
+//     brand: "Penti",
+//   },
+//   {
+//     id: 10,
+//     name: "https://gtex-sms-verification.netlify.app/Penti%20East%20Point",
+//     brand: "Penti",
+//   },
+//   {
+//     id: 10,
+//     name: "https://gtex-sms-verification.netlify.app/Penti%20City%20Mall",
+//     brand: "Penti",
+//   },
+//   {
+//     id: 11,
+//     name: "https://gtex-sms-verification.netlify.app/Penti%20Batumi",
+//     brand: "Penti",
+//   },
+//   {
+//     id: 12,
+//     name: "https://gtex-sms-verification.netlify.app/Penti%20Axis",
+//     brand: "Penti",
+//   },
+//   {
+//     id: 13,
+//     name: "https://gtex-sms-verification.netlify.app/Penti%20AgroHub",
+//     brand: "Penti",
+//   },
+//   {
+//     id: 14,
+//     name: "https://gtex-sms-verification.netlify.app/OVS%20Tbilisi%20Mall",
+//     brand: "OVS",
+//   },
+//   {
+//     id: 15,
+//     name: "https://gtex-sms-verification.netlify.app/OVS%20Pekini",
+//     brand: "OVS",
+//   },
+//   {
+//     id: 16,
+//     name: "https://gtex-sms-verification.netlify.app/OVS%20Grand%20Mall",
+//     brand: "OVS",
+//   },
+//   {
+//     id: 17,
+//     name: "https://gtex-sms-verification.netlify.app/OVS%20East%20Point",
+//     brand: "OVS",
+//   },
+//   {
+//     id: 18,
+//     name: "https://gtex-sms-verification.netlify.app/OVS%20City%20Mall",
+//     brand: "OVS",
+//   },
+//   {
+//     id: 19,
+//     name: "https://gtex-sms-verification.netlify.app/Matalan%20City%20Mall",
+//     brand: "Matalan",
+//   },
+//   {
+//     id: 20,
+//     name: "https://gtex-sms-verification.netlify.app/Kiabi%20Metro%20City",
+//     brand: "Kiabi",
+//   },
+//   {
+//     id: 21,
+//     name: "https://gtex-sms-verification.netlify.app/Kiabi%20East%20Point",
+//     brand: "Kiabi",
+//   },
+//   {
+//     id: 22,
+//     name: "https://gtex-sms-verification.netlify.app/English%20Home%20Rustavi",
+//     brand: "English Home",
+//   },
+//   {
+//     id: 23,
+//     name: "https://gtex-sms-verification.netlify.app/English%20Home%20Grand%20Mall",
+//     brand: "English Home",
+//   },
+//   {
+//     id: 24,
+//     name: "https://gtex-sms-verification.netlify.app/English%20Home%20East%20Point",
+//     brand: "English Home",
+//   },
+//   {
+//     id: 25,
+//     name: "https://gtex-sms-verification.netlify.app/English%20Home%20City%20Mall",
+//     brand: "English Home",
+//   },
+
+//   {
+//     id: 26,
+//     name: "https://gtex-sms-verification.netlify.app/Enza%20Home%20Agmashenebeli",
+//     brand: "Enza Home",
+//   },
+//   {
+//     id: 27,
+//     name: "https://gtex-sms-verification.netlify.app/Enza%20Home%20East%20Point",
+//     brand: "Enza Home",
+//   },
+//   {
+//     id: 28,
+//     name: "https://gtex-sms-verification.netlify.app/Enza%20Home%20City%20Mall",
+//     brand: "Enza Home",
+//   },
+//   {
+//     id: 29,
+//     name: "https://gtex-sms-verification.netlify.app/Enza%20HomeG%20rand%20Mall",
+//     brand: "Enza Home",
+//   },
+//   {
+//     id: 30,
+//     name: "https://gtex-sms-verification.netlify.app/Terranova%20Agmashenebeli",
+//     brand: "Terranova",
+//   },
+//   {
+//     id: 31,
+//     name: "https://gtex-sms-verification.netlify.app/Terranova%20East%20Point",
+//     brand: "Terranova",
+//   },
+//   {
+//     id: 32,
+//     name: "https://gtex-sms-verification.netlify.app/Terranova%20Metro%20City",
+//     brand: "Terranova",
+//   },
+//   {
+//     id: 33,
+//     name: "https://gtex-sms-verification.netlify.app/Terranova%20Tbilisi%20Central",
+//     brand: "Terranova",
+//   },
+// ];
+
+// for (let i = 0; i <= branches.length - 1; i++) {
+//   console.log(branches[i].name);
+// }
